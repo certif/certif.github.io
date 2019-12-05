67167d01aec56e2e38d25b5d2545b61ac3244167
Oleksandr Tkachov <certifis@gmail.com>
2019-12-05 22:19:26 +0200

    feat: add base html file

---
+++ portfolio/script.js
@@ -0,0 +1,3 @@
+const educationTitle = document.querySelector(".education-title");
+const educationInfo = document.querySelector(".education_list");
+const educationControl = document.querySelector(".education_control");
